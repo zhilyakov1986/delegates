2fa07acd005e6edd276b528a286132bdd9f0a99a
Andrey Zhilyakov <zhilyakov1986@gmail.com>
2019-01-01 18:18:34 -0500

    adding events to the car class

--- CarDelegate/Program.cs
+++ CarDelegate/Program.cs
@@ -6,46 +6,66 @@ namespace CarDelegate
     {
         private static void Main(string[] args)
         {
-            // create car
-            Car c1 = new Car(10, 100, "SlugBug");
-
-            // tell the car which method to call
-            // when it wants us send us messages
-            c1.RegisterWithCarEngine(OnCarEngineEvent);
-
-            // hold onto delegate object, so we can unregistered it later
-            Car.CarEngineHandler handler2 = OnCarEngineEvent2;
-            c1.RegisterWithCarEngine(handler2);
-            // speed up (this will trigger the events)
-            Console.WriteLine("***Speeding Up***");
+            Car c1 = new Car();
+            //register the simple method name
+            c1.RegisterWithCarEngine(CallMeHere);
+            Console.WriteLine("***Speeding UP***");
             for (int i = 0; i < 6; i++)
             {
                 c1.Accelerate(20);
             }
-            // unregistered second handler
-            c1.UnregisterWithCarEngine(handler2);
-
-            // We won't see the "uppercase" message anymore!
-            Console.WriteLine("***Speeding Up***");
-            for (int i = 0; i < 6; i++)
-            {
-                c1.Accelerate(20);
-            }
+            c1.UnregisterWithCarEngine(CallMeHere);
         }
 
 
-        private static void OnCarEngineEvent2(string msgforcaller)
+        private static void CallMeHere(string msgforcaller)
         {
-            Console.WriteLine($"=>{msgforcaller.ToUpper()}");
+            throw new NotImplementedException();
         }
 
-        // this is the target for incoming events
-        public static void OnCarEngineEvent(string msg)
-        {
-            Console.WriteLine("\n*** Message From Car Object ***");
-            Console.WriteLine($"=>{msg}");
-            Console.WriteLine("********************************\n");
-        }
+
+        //--------------------------------------------------------------
+        //{
+        //    // create car
+        //    Car c1 = new Car(10, 100, "SlugBug");
+
+        //    // tell the car which method to call
+        //    // when it wants us send us messages
+        //    c1.RegisterWithCarEngine(OnCarEngineEvent);
+
+        //    // hold onto delegate object, so we can unregistered it later
+        //    Car.CarEngineHandler handler2 = OnCarEngineEvent2;
+        //    c1.RegisterWithCarEngine(handler2);
+        //    // speed up (this will trigger the events)
+        //    Console.WriteLine("***Speeding Up***");
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        c1.Accelerate(20);
+        //    }
+        //    // unregistered second handler
+        //    c1.UnregisterWithCarEngine(handler2);
+
+        //    // We won't see the "uppercase" message anymore!
+        //    Console.WriteLine("***Speeding Up***");
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        c1.Accelerate(20);
+        //    }
+        //}
+
+
+        //private static void OnCarEngineEvent2(string msgforcaller)
+        //{
+        //    Console.WriteLine($"=>{msgforcaller.ToUpper()}");
+        //}
+
+        //// this is the target for incoming events
+        //public static void OnCarEngineEvent(string msg)
+        //{
+        //    Console.WriteLine("\n*** Message From Car Object ***");
+        //    Console.WriteLine($"=>{msg}");
+        //    Console.WriteLine("********************************\n");
+        //}
 
     }
 }
---
+++ CarEvents/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CarEvents
+{
+    internal class Car
+    {
+        // This delegate works in conjunction with the Car events
+        public delegate void CarEngineHandler(string msg);
+
+        // This car sends these events
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is car alive or dead?
+        private bool _carIsDead;
+
+        // Class constructors
+        public Car()
+        {
+
+        }
+
+        public Car(int currentSpeed, int maxSpeed, string petName)
+        {
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = petName;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_carIsDead)
+            {
+                Exploded?.Invoke("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // almost dead
+                if (10 == MaxSpeed - CurrentSpeed)
+                {
+                    AboutToBlow?.Invoke("Careful buddy! Gonna blow!");
+                }
+                // still ok!
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Current speed = {CurrentSpeed}");
+                }
+            }
+        }
+
+
+    }
+}
+
+
