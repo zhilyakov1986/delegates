c08e64cfbabcb6f86ae2294538b47b0a5d5be75f
Andrey Zhilyakov <zhilyakov1986@gmail.com>
2019-01-01 16:04:08 -0500

    learning about delegate invocation list

---
+++ CarDelegate/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is car alive or dead?
+        private bool carIsDead;
+
+        // Class constructors
+        public Car()
+        {
+
+        }
+
+        public Car(int currentSpeed, int maxSpeed, string petName)
+        {
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = petName;
+        }
+
+        // Define a delegate type.
+        public delegate void CarEngineHandler(string msgForCaller);
+        // Define a member variable for this delegate
+        private CarEngineHandler listOfHandlers;
+        // Add registration function for the caller
+        public void RegisterWithCarEngine(CarEngineHandler methodCall)
+        {
+            // use for single cast =
+            //listOfHandlers = methodCall;
+            // to enable multi cast use +=
+            listOfHandlers += methodCall;
+        }
+
+        public void UnregisterWithCarEngine(CarEngineHandler methodCall)
+        {
+            listOfHandlers -= methodCall;
+        }
+        // implement the Accelerate() method to invoke the delegate's
+        // invocation list under the correct circumstances
+        public void Accelerate(int delta)
+        {
+            // if car is dead send the message
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("Sorry, this car is dead...");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // is this car almost dead?
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna Blow!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine($"CurrentSpeed = {CurrentSpeed}");
+                }
+            }
+        }
+    }
+}
+
--- CarDelegate/Program.cs
+++ CarDelegate/Program.cs
@@ -2,11 +2,50 @@
 
 namespace CarDelegate
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create car
+            Car c1 = new Car(10, 100, "SlugBug");
+
+            // tell the car which method to call
+            // when it wants us send us messages
+            c1.RegisterWithCarEngine(OnCarEngineEvent);
+
+            // hold onto delegate object, so we can unregistered it later
+            Car.CarEngineHandler handler2 = OnCarEngineEvent2;
+            c1.RegisterWithCarEngine(handler2);
+            // speed up (this will trigger the events)
+            Console.WriteLine("***Speeding Up***");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+            // unregistered second handler
+            c1.UnregisterWithCarEngine(handler2);
+
+            // We won't see the "uppercase" message anymore!
+            Console.WriteLine("***Speeding Up***");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
         }
+
+
+        private static void OnCarEngineEvent2(string msgforcaller)
+        {
+            Console.WriteLine($"=>{msgforcaller.ToUpper()}");
+        }
+
+        // this is the target for incoming events
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n*** Message From Car Object ***");
+            Console.WriteLine($"=>{msg}");
+            Console.WriteLine("********************************\n");
+        }
+
     }
 }
